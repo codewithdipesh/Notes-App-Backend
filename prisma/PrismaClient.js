6fdc7c390513f116e17750b99a86111ac8d5aa51
Dipesh Pal <codewithdipesh2022@gmail.com>
2024-06-05 01:12:02 +0530

    added connect function in prisma client file

--- prisma/PrismaClient.js
+++ prisma/PrismaClient.js
@@ -2,3 +2,12 @@ import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient()
 
+export const connectDB = async () =>{
+    try {
+        prisma.$connect()
+        console.log("Connected to the database successfully")
+    } catch (error) {
+        console.error("Failed to connect to the database:", error);
+        throw error;
+    }
+}
