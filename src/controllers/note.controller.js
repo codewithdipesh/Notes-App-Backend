ff7fdc7e1c98c4db16caf39c91c3dc9a4a41b659
Dipesh Pal <codewithdipesh2022@gmail.com>
2024-06-10 14:29:37 +0530

    finished note controller logic

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -31,5 +31,98 @@ export const createNote = async(req,res,next)=>{
    }
 }
 //update note
+export const updateNote = async(req,res,next)=>{
+try {
+    const noteId = parseInt(req.params.noteId)
+    if(!noteId){
+      throw new ApiError(400,"NoteId is required")
+    }
+    const {title,content} = req.body
+    let updatedNote
+    if(!title && !content){
+      throw new ApiError(400,'Nothing to be updated')
+      }
+    else if(!title){
+      updatedNote = await prisma.note.update({
+        where:{id:noteId},
+        data:{content :content}
+      })
+    }
+    else if(!content){
+      updatedNote = await prisma.note.update({
+        where:{id:noteId},
+        data:{title :title}
+      })
+    }
+    else{
+      updatedNote = await prisma.note.update({
+        where:{id:noteId},
+        data:{
+          title :title,
+          content :content
+        }
+      })
+    }
+  
+    if(!updateNote){
+      throw new ApiError(500,"Unable to update note")
+    }
+  
+    return res.status(200).json(
+      new ApiResponse(200,"Note updated successfully",{updatednote : updatedNote})
+    )
+      
+} catch (e) {
+  throw new ApiError(500,"Something went wrong in server\n"+e?.message)
+}
+  
+}
 //delete note
+export const deleteNote = async(req,res,next)=>{
+  try {
+    const noteId = parseInt(req.params.noteId)
+    if(!noteId){
+      throw new ApiError(400,"NoteId is required")
+    }
+    const deletedNote = await prisma.note.delete({
+      where:{id:noteId}
+    })
+    if(!deletedNote){
+      throw new ApiError(500,"Unable to delete note")
+    }
+  
+    return res.status(200).json(
+      new ApiResponse(200,"Note deleted successfully",{})
+    )
+      
+  } catch (e) {
+    throw new ApiError(500,"Something went wrong in server\n"+e?.message)
+  }
+
+}
+
 //get all notes
+export const getAllnotes = async(req,res,next)=>{
+  try {
+    const notes = await prisma.note.findMany({
+      where:{authorId:req.user.id},
+      select:{
+        id:true,
+        title:true,
+        content:true,
+        createdAt:true,
+        updatedAt:true
+      }
+    })
+    if(!notes || notes.length === 0){
+      throw new ApiError(400,"There is no Notes present")
+    }
+     
+    return res.status(200).json(
+      new ApiResponse(200,"All Notes fetched successfully",{notes :notes})
+    )
+  } catch (e) {
+    throw new ApiError(500,'Something went wrong\n'+e?.message)
+  }
+  
+}
--- src/routers/note.route.js
+++ src/routers/note.route.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
-import { createNote } from "../controllers/note.controller.js";
+import { createNote, getAllnotes, updateNote } from "../controllers/note.controller.js";
 
 const router = Router()
 
+router.route("/").get(verifyJwt,getAllnotes)
 router.route("/create-new").post(verifyJwt,createNote)
+router.route("/:noteId").patch(verifyJwt,updateNote)
+
 
 export default router
