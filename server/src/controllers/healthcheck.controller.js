c6785d26100dbad45fcf8a9d7f0e0ea1fcfa5fb0
Dipesh Pal <codewithdipesh2022@gmail.com>
2024-06-22 22:57:44 +0530

    testing git action

--- server/src/controllers/healthcheck.controller.js
+++ server/src/controllers/healthcheck.controller.js
@@ -2,6 +2,6 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 
 export const healthcheck = async(req,res,next)=>{
     return res.status(200).json(
-        new ApiResponse(200,"Server akdam taka tak ")
+        new ApiResponse(200,"Server is running healthy")
     )
 }
